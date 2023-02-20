20a366fda78cd11a2f71cff7a9a1e11333929f6a
Andy Phan <andyphan@live.com.au>
2023-02-20 12:26:53 +1030

    Implemented function to declare winner by poping up a message and blurring the background. Also implemented error message when player selects an already selected square

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -62,29 +62,27 @@ const gameController = (function() {
     // Function to control player's selection of squares
     const _playerTurn = (index) => {
         if(gameBoard.getSquare(index) !== null) {
-            console.log('Taken.');
+            displayController.wrongSelection();
         } else {
+            displayController.rightSelection();
             gameBoard.setSquare(index, _players[_round++ % 2]);
         }
         
         if(_checkWin()) {
-            console.log(_players[(_round - 1) % 2].getMark() + " is the winner.");
+            displayController.declareWinner(((_round) % 2) + 1);
         } else if(_round === 10) {
             console.log("DRAW");
         }
+
+        displayController.updateTurn();
     }
 
     // Check if there is a winning combination
     const _checkWin = () => {
         const winCombinations = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6],            // Diagonals
         ];
         
         const currentPlayerMark = _players[(_round - 1) % 2].getMark();
@@ -101,6 +99,9 @@ const gameController = (function() {
         gameBoard.clearBoard();
     }
 
+    // Get the current round
+    const getRound = () => _round;
+
     // Initialise click event for each square
     const _init = (() => {
         for(let i = 0; i < _boardHTML.length; i++) {
@@ -108,9 +109,61 @@ const gameController = (function() {
         }
 
         document.querySelector('.restart-btn').addEventListener('click', _restartGame);
+        document.querySelector('.modal-toggle').addEventListener('click', _restartGame);
     })();
+
+    return {
+        getRound
+    }
 })();
 
+// Display Controller Module
 const displayController = (function() {
+    // Show which players turn it is
+    const _turnHTML = document.querySelector('.player-turn > h1');
+    const updateTurn = () => {
+        if(gameController.getRound() % 2) {
+            _turnHTML.innerText = "Player 1's Turn";
+        } else {
+            _turnHTML.innerText = "Player 2's Turn";
+        }
+    }
 
+    // Declare the winner
+    const containerHTML = document.querySelector('.container');
+    const modalWrapper = document.querySelector('.modal-wrapper');
+    const modalButtons = document.querySelector('.modal-toggle');
+    const modelTitle = document.querySelector('.modal-title');
+    const declareWinner = (playerNum) => {
+        modelTitle.innerText = 'Player ' + playerNum + ' Wins!'
+        _toggleWinModal();
+    };
+
+    const _toggleWinModal = () => {
+        containerHTML.classList.toggle('modal--active');
+        modalWrapper.classList.toggle('modal--active');
+    }
+
+    // Toggles error for selecting an already selected square
+    const wrongSelectionHTML = document.querySelector('.selection-err');
+    const wrongSelection = () => {
+        wrongSelectionHTML.classList.add('wrong-selection');
+    }
+
+    const rightSelection = () => {
+        wrongSelectionHTML.classList.remove('wrong-selection');
+    }
+
+    // Initialise buttons to play again
+    const _init = (() => {
+        modalButtons.addEventListener('click', _toggleWinModal);
+        modalButtons.addEventListener('click', updateTurn);
+    })();
+
+    return {
+        updateTurn,
+        declareWinner,
+        wrongSelection,
+        rightSelection
+    }
 })();
